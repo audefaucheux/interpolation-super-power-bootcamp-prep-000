ca2b3f86aafcae4d5ed9a3ede317dec182973a65
Aude Faucheux <aude53@gmail.com>
2019-07-13 18:57:44 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(color_array)
-  puts "R: #{color_array[0]},
-        O: #{color_array[1]},
+  print "R: #{color_array[0]}, O: #{color_array[1]},
         Y: #{color_array[2]},
         G: #{color_array[3]},
         B: #{color_array[4]},
